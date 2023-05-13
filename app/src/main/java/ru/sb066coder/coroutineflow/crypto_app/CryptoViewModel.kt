192153e6beedd2402a0e5966f8e5fb8651782b7a
sergey <sergey066@yandex.ru>
2023-05-13 12:34:12 +0300

    Mapping in flow added, flow connected directly to the field asLiveData

--- app/src/main/java/ru/sb066coder/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/ru/sb066coder/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,47 +1,15 @@
 package ru.sb066coder.coroutineflow.crypto_app
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
+import androidx.lifecycle.*
+import kotlinx.coroutines.flow.*
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-//        viewModelScope.launch {
-//            repository.getCurrencyList()
-//                .onStart {
-//                    val currentState = _state.value
-//                    if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-//                        _state.value = State.Loading
-//                    }
-//                }
-//                .filter { it.isNotEmpty() }
-//                .onEach { _state.value = State.Content(it) }
-//                .collect()
-//        }
-        repository.getCurrencyList()
-            .onStart {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
-                }
-            }
-            .filter { it.isNotEmpty() }
-            .onEach { _state.value = State.Content(it) }
-            .launchIn(viewModelScope)
-    }
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State }
+        .onStart { emit(State.Loading) }
+        .asLiveData()
 }
